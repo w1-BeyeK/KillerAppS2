b36a8d0486e2f1ad993af5db5b6401847fdd1dac
Kevin Beye <kbeye1999@hotmail.com>
2019-05-27 10:36:01 +0200

    App modules

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,14 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '../_models';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+    constructor(
+        private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<User[]>(`https://localhost:44317/api/user`);
+    }
+}
